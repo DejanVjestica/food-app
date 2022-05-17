db421b64d1ebe8a13adb6b9784d627b22193a3aa
Dejan Vjestica <dvjestica@gmail.com>
2022-05-17 15:56:37 +0200

    Add cart button to header and scss

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
-import styles from './Header.module.scss'
+import headerStyles from './Header.module.scss'
+import CartStyles from './CartButton.module.scss'
 
 import { Stage } from '../Stage/Stage'
 import { Button } from '../../UI/Button/Button'
@@ -12,9 +15,23 @@ export const Header = () => {
 	}
 	return (
 		<>
-			<Wrapper as="header" className={styles.header}>
-				<h1 className={styles.headline}>Food app</h1>
-				<Button onClick={onClickHandler}>Cart</Button>
+			<Wrapper as="header" className={headerStyles.header}>
+				<h1 className={headerStyles.headline}>
+					Food app
+				</h1>
+				<Button
+					className={CartStyles.button}
+					onClick={onClickHandler}>
+					<span className={CartStyles.icon}>
+						<FontAwesomeIcon
+							icon={faCartShopping}
+						/>
+					</span>
+					<span>Your cart</span>
+					<span className={CartStyles.badge}>
+						3
+					</span>
+				</Button>
 			</Wrapper>
 			<Stage></Stage>
 		</>
