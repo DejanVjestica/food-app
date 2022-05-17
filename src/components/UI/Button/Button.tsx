3fe0f2668a4bd3eee9cb29646b33ff43024c4e8f
Dejan Vjestica <dvjestica@gmail.com>
2022-05-17 15:56:03 +0200

    Remove scss file, remove span from button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 
-import styles from './Button.module.scss'
-
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = (props: ButtonProps) => {
-	return (
-		<button className={styles.button} {...props}>
-			{props.children}
-		</button>
-	)
+	return <button {...props}>{props.children}</button>
 }
