f9ad9fda9b5ad846444a89414c16e76d491a4d8a
Dejan Vjestica <dvjestica@gmail.com>
2022-05-17 14:56:33 +0200

    Add polymorphic component wrapper

---
+++ src/components/Helpers/Wrapper/Wrapper.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type WrapperOwnProps<E extends React.ElementType> = {
+	children: React.ReactNode
+	as?: E
+}
+
+type WrapperProps<E extends React.ElementType> = WrapperOwnProps<E> &
+	Omit<React.ComponentProps<E>, keyof WrapperOwnProps<E>>
+
+export const Wrapper = <E extends React.ElementType = 'div'>(
+	props: WrapperProps<E>
+) => {
+	const { children, as: Component = 'div', ...rest } = props
+	return <Component {...rest}>{children}</Component>
+}
