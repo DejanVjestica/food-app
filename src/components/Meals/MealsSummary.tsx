0e8dbfeb01bac49f4693be1a11943eda971484e2
Dejan Vjestica <dvjestica@gmail.com>
2022-05-18 16:36:03 +0200

    Add mealssummary and styles

---
+++ src/components/Meals/MealsSummary.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from './MealsSummary.module.scss'
+
+export const MealsSummary = () => {
+	return (
+		<section className={styles.summary}>
+			<h2>Lorem Ipsum dolor</h2>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`&apos;`s standard
+				dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+				has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+				popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+				publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			</p>
+		</section>
+	)
+}
