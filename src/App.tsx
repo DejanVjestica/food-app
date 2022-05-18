0f7b9ea1a93aa25137a2c6d621948df2cbaecf14
Dejan Vjestica <dvjestica@gmail.com>
2022-05-18 16:35:08 +0200

    Refactor app add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 import { Header } from './components/Layout/Header/Header'
 import { Stage } from './components/Layout/Stage/Stage'
+import { Meals } from './components/Meals/Meals'
+import { Wrapper } from './components/Helpers/Wrapper/Wrapper'
+
+import styles from './App.module.scss'
 
 export const App = () => {
 	return (
 		<>
 			<Header />
 			<Stage />
+			<Wrapper as="main" className={styles.main}>
+				<Meals />
+			</Wrapper>
 		</>
 	)
 }
