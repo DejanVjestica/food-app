fbac80a64c3a72d867dcca32b80dd7e78bc00bdf
Dejan Vjestica <dvjestica@gmail.com>
2022-05-19 11:38:46 +0200

    Refactor available meals

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -3,16 +3,14 @@ import React from 'react'
 import styles from './AvailableMeals.module.scss'
 import Meals from './Meals.data.json'
 
-import { MealItem } from './MealItem'
+import { MealItem } from './MealItem/MealItem'
 
 export const AvailableMeals = () => {
+	const meals = Meals.map((meal) => <MealItem key={meal.id} meal={meal} />)
+
 	return (
 		<section className={styles['available-meals']}>
-			<ul className={styles['available-meals__list']}>
-				{Meals.map((meal) => {
-					return <MealItem key={meal.id} meal={meal}></MealItem>
-				})}
-			</ul>
+			<ul className={styles['available-meals__list']}>{meals}</ul>
 		</section>
 	)
 }
