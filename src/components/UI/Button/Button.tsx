2880493f34eaced4a8f30e6b85efe15deb58bfe0
Dejan Vjestica <dvjestica@gmail.com>
2022-05-23 15:13:32 +0200

    Refactor button ts

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+	children: React.ReactNode
+} & Omit<React.ComponentProps<'button'>, 'children'>
 
 export const Button = (props: ButtonProps) => {
 	return <button {...props}>{props.children}</button>
