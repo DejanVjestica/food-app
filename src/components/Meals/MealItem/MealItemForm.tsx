3825e75ea2fcaa7ef62eb3583de9d291a621457d
Dejan Vjestica <dvjestica@gmail.com>
2022-05-25 11:23:19 +0200

    Add form error validation

--- src/components/Meals/MealItem/MealItemForm.tsx
+++ src/components/Meals/MealItem/MealItemForm.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 
 // components
 import { Button } from '../../UI/Button/Button'
@@ -18,13 +18,22 @@ type MealItemFormProps = {
 }
 
 export const MealItemForm = ({ meal }: MealItemFormProps) => {
+	const [isValid, setIsValid] = useState<boolean>(true)
 	const { addItem } = useContext(CartContext)
 	const quantityRef = useRef<HTMLInputElement>(null)
 
 	const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
 
-		const quantity = +quantityRef.current!.value
+		const addedQuantity = quantityRef.current!.value
+		const quantity = +addedQuantity
+
+		if (addedQuantity.trim().length === 0 || quantity <= 0) {
+			setIsValid(false)
+			return
+		} else {
+			setIsValid(true)
+		}
 
 		const newMeal: MealItemExtendedType = {
 			...meal,
@@ -37,6 +46,7 @@ export const MealItemForm = ({ meal }: MealItemFormProps) => {
 	return (
 		<form className={styles.form} onSubmit={submitHandler}>
 			<Input ref={quantityRef} type="number" min="1" max="5" step="1" value="1" label="Amount" id="cart" />
+			{!isValid && <p className={styles.error}>Please enter a valid amount</p>}
 			<Button type="submit">+ Add to cart</Button>
 		</form>
 	)
