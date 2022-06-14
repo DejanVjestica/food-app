fba5a55c9b039b70d316b9936fef52a356052169
Dejan Vjestica <dvjestica@gmail.com>
2022-06-14 16:59:26 +0200

    Cleanup Img component

--- src/components/UI/Img/Img.tsx
+++ src/components/UI/Img/Img.tsx
@@ -1,28 +1,24 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { SrcSetContext } from '../../../context/srcSet-context.tsx/srcSet-context'
 
-type SrcSet = {
-	src: string
-	imageWidth: string
-}
+import { SrcSetItem } from '../../../types/srcSet.types'
 
 type ImgProps = {
-	srcSet?: string | SrcSet[]
+	srcSet?: string | SrcSetItem[]
 } & Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'srcSet'>
 
 export const Img = (props: ImgProps) => {
 	const context = useContext(SrcSetContext)
 	const [srcSet, setSrcSet] = useState<string>('')
 
-	if (!props.srcSet) {
-		return <img {...props} srcSet={srcSet}></img>
-	}
+	// TODO fix typescript to be able not to pass srcSet as empty string
+	if (!props.srcSet) return <img {...props} srcSet={srcSet}/>
 
 	useEffect(() => {
 		if (typeof props.srcSet === 'string') {
 			setSrcSet(props.srcSet)
 		} else {
-			setSrcSet(context.generateSrcSet(props.srcSet as SrcSet[]))
+			setSrcSet(context.generateSrcSet(props.srcSet as SrcSetItem[]))
 		}
 	}, [srcSet])
 
