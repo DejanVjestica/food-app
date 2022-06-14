cf989c2007ee12f3ac951c8d2af543656be1f3f9
Dejan Vjestica <dvjestica@gmail.com>
2022-06-14 12:28:18 +0200

    Refactor button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from './Button.module.scss'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-	children: React.ReactNode
+	children: React.ReactNode,
+	variant?: 'primary' | 'secondary' | 'icon' | 'simple'
 } & Omit<React.ComponentProps<'button'>, 'children'>
 
 export const Button = (props: ButtonProps) => {
-	return <button {...props}>{props.children}</button>
+	const { variant } = props
+
+	return <button className={[styles.default, styles[`${variant}`]].join(' ')} {...props}>{props.children}</button>
 }
