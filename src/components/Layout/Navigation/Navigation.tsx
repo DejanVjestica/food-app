213fea3a977fcf964a7dcaf5f4e803bcc834485b
Dejan Vjestica <dvjestica@gmail.com>
2022-06-17 16:05:51 +0200

    Add navigation ocmponent

--- src/components/Layout/Navigation/Navigation.tsx
+++ src/components/Layout/Navigation/Navigation.tsx
@@ -1,44 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 // components
 import { ListItem } from '../../UI/ListItem/ListItem'
+import { Button } from '../../UI/Button/Button'
 
 // styles
 import styles from './Navigation.module.scss'
+
+// context
+import { UserContext } from '../../../context/User/user-context'
 
 type NavigationProps = {
 	variant: 'header' | 'footer'
 }
 
-const NavItems = [
-	{
-		id: 1,
-		link: {
-			href: '#',
-			title: 'Home'
-		},
-		text: 'Home'
-	},
-	{
-		id: 2,
-		link: {
-			href: '#',
-			title: 'Restaurant'
-		},
-		text: 'Restaurant'
-	},
-	{
-		id: 3,
-		link: {
-			href: '#',
-			title: 'Login'
-		},
-		text: 'Login'
-	}
-]
 export const Navigation = ({ variant }: NavigationProps) => {
-	const items = NavItems.map(({ link, text, id }) => (
-		<ListItem key={id} link={link} text={text} />
+	const { openUserModal } = useContext(UserContext)
+
+	const NavItems = [
+		{
+			id: 2,
+			text: 'Restaurant',
+			onClickHandler: () => {
+				console.log('Restaurant')
+			}
+		},
+		{
+			id: 3,
+			text: 'Login',
+			onClickHandler: openUserModal
+		}
+	]
+	const items = NavItems.map(({ id, text, onClickHandler }) => (
+		<ListItem key={id}>
+			<Button variant='simple' modifier='is-color-white' onClick={onClickHandler}><span>{text}</span></Button>
+		</ListItem>
 	))
 
 	return (
