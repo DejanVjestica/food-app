ee158db48fba0a662d4fd326a24188743549c08a
Dejan Vjestica <dvjestica@gmail.com>
2022-06-21 10:21:57 +0200

    Add onSubmit to form component

--- src/components/UI/Form/Form.tsx
+++ src/components/UI/Form/Form.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 type FormProps = React.FormHTMLAttributes<HTMLFormElement> & {
-	children: React.ReactNode
+	children: React.ReactNode,
+	onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void
+
 } & Omit<React.ComponentProps<'form'>, 'children'>
 
-export const Form = ({ children }: FormProps) => {
-	return <form>{children}</form>
+export const Form = ({ children, onSubmit }: FormProps) => {
+	return <form onSubmit={onSubmit}>{children}</form>
 }
