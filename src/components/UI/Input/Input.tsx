0cbc0e1d3fa5dd62a057a8004d56d2ef7a4c9323
Dejan Vjestica <dvjestica@gmail.com>
2022-06-21 10:24:06 +0200

    Add validation to input

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,21 +1,45 @@
-/* eslint-disable react/display-name */
-import React, { forwardRef } from 'react'
+/* eslint-disable no-tabs */
+import React, { useEffect } from 'react'
+
+// custom hooks
+import { useInput } from '../../../hooks/use-input'
+
+// types
+import { UseInputConfigType, retrieveValuesParams } from '../../../hooks/use-input.types'
 
 // Styles
 import styles from './Input.module.scss'
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 	label?: string
-	id?: string
+	id?: string,
+	retrieveValues: ({ value, hasError, resetState }:retrieveValuesParams) => void,
+	useInputConfig: UseInputConfigType
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props: InputProps, ref) => {
-	const { label, ...newProps } = props
+export const Input = (props: InputProps) => {
+	const { label, useInputConfig, retrieveValues, ...newProps } = props
+
+	const {
+		value,
+		hasError,
+		onChangeHandler,
+		onBlurHandler,
+		resetState
+
+	} = useInput(useInputConfig as UseInputConfigType)
+
+	// Effects
+	useEffect(() => {
+		if (!retrieveValues) return
+		retrieveValues({ value, hasError, resetState })
+	}, [retrieveValues, value, hasError])
 
 	return (
 		<div className={styles.input__wrapper}>
 			{label && <label htmlFor={newProps.id}>{label}</label>}
-			<input ref={ref} {...newProps} />
+			<input {...newProps} onChange={onChangeHandler} onBlur={onBlurHandler} />
+			{hasError && <p className={styles.error}>{useInputConfig.errorText}</p>}
 		</div>
 	)
-})
+}
