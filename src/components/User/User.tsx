bc924a721ba1f78bf78151355e527edde5ad6086
Dejan Vjestica <dvjestica@gmail.com>
2022-06-23 15:59:34 +0200

    Add useAuthState hook, add profile

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -15,31 +15,36 @@ import { Login } from './Login/Login'
 import { Register } from './Register/Register'
 import { Modal } from '../UI/Modal/Modal'
 import { Button } from '../UI/Button/Button'
+import { Profile } from './Profile/Profile'
+
+// Firebase
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth, logout } from '../../firebase'
 
 export const User = () => {
+	// authState
+	const [user, loading] = useAuthState(auth)
+
+	// Context
 	const { isLoginOpen, isRegisterOpen, openLogin, openRegister, closeModal } = useContext(UserContext)
-
-	const loginHandler = () => {
-		openLogin()
-	}
-
-	const registerHandler = () => {
-		openRegister()
-	}
 
 	return <Modal element='#user-modal-root'>
 		<div className={styles.wrapper}>
 			<Button variant='icon' onClick={closeModal} >
 				<span className={styles['cancel-icon']}><FontAwesomeIcon icon={faClose} /></span>
 			</Button>
-
-			{!isLoginOpen && !isRegisterOpen && <h2>Mein Account</h2>}
-			{!isLoginOpen && !isRegisterOpen && <div className={styles.controls}>
-				<Button variant='secondary' onClick={loginHandler}>Login</Button>
-				<Button variant='secondary' onClick={registerHandler}>Register new account</Button>
-			</div>}
-			{isLoginOpen && <Login />}
-			{isRegisterOpen && <Register />}
+			{!user && <>
+				{!isLoginOpen && !isRegisterOpen && <h2>Mein Account</h2>}
+				{!isLoginOpen && !isRegisterOpen && <div className={styles.controls}>
+					<Button variant='secondary' onClick={openLogin}>Login</Button>
+					<Button variant='secondary' onClick={openRegister}>Register new account</Button>
+				</div>}
+				{isLoginOpen && <Login />}
+				{isRegisterOpen && <Register />}
+			</>}
 		</div>
+		{user && <Profile user={user}></Profile>}
+		{loading && <p>Please wait is loading</p>}
+		{user && <Button variant='primary' onClick={logout}>Logout</Button>}
 	</Modal>
 }
