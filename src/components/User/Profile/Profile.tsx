f55ffd262e00fdf5781d32529a3701acb549c769
Dejan Vjestica <dvjestica@gmail.com>
2022-06-23 15:59:02 +0200

    Add profile component

---
+++ src/components/User/Profile/Profile.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+// Firebase
+import {
+	User
+} from 'firebase/auth'
+
+// components
+import { Img } from '../../UI/Img/Img'
+import { Button } from '../../UI/Button/Button'
+
+// styles
+import styles from './Profile.module.scss'
+
+type ProfileProps = {
+	user: User | null | undefined
+}
+
+export const Profile = ({ user }: ProfileProps) => {
+	const userName = user?.displayName || user?.email
+	const userImg = user?.photoURL || undefined
+
+	return (
+		<div className={styles.profile}>
+			{userImg && <Img srcSet={userImg} alt='profile image' />}
+			<div className={styles.info}>
+				<h2>{userName}</h2>
+				<Button variant='simple'>Pesonal data</Button>
+			</div>
+		</div>
+	)
+}
