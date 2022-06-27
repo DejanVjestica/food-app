0d6da71239ee68f92f309f5697817389af70c468
Dejan Vjestica <dvjestica@gmail.com>
2022-06-27 13:26:54 +0200

    Add restauarants component

--- src/components/Restaurants/Restaurants.tsx
+++ src/components/Restaurants/Restaurants.tsx
@@ -5,6 +5,7 @@ import { Wrapper } from '../Helpers/Wrapper/Wrapper'
 import { Img } from '../UI/Img/Img'
 import { ListItem } from '../UI/ListItem/ListItem'
 import { Button } from '../UI/Button/Button'
+import { Spinner } from '../UI/Spinner/Spinner'
 
 // styles
 import styles from './Restaurants.module.scss'
@@ -14,12 +15,12 @@ import { getDatabase, ref as databaseRef } from 'firebase/database'
 import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage'
 import { useList } from 'react-firebase-hooks/database'
 
-export const Restaurants = (): JSX.Element => {
+export const Restaurants = () => {
 	const dbRef = databaseRef(getDatabase(), 'restaurants')
 	const storage = getStorage()
 
 	// state
-	const [snapshots] = useList(dbRef)
+	const [snapshots, loading, error] = useList(dbRef)
 	const restaurantsList = snapshots?.map(snapshot => snapshot.val())
 
 	const restaurants = restaurantsList?.map((restaurant) => {
@@ -29,7 +30,7 @@ export const Restaurants = (): JSX.Element => {
 			getDownloadURL(storageRef(storage, imagePath)).then(url => {
 				const img = document.getElementById(`${restaurant.name}`)
 				img?.setAttribute('src', url)
-			})
+			}).catch(err => console.log(err))
 		}
 		getImageUrl()
 
@@ -43,12 +44,12 @@ export const Restaurants = (): JSX.Element => {
 			</ListItem>
 		)
 	})
-
 	return (
 		<div className={styles.restaurants}>
-			<ul className={styles['restaurants-list']}>
+			{loading && <Spinner></Spinner>}
+			{!loading && restaurants && <ul className={styles['restaurants-list']}>
 				{restaurants}
-			</ul>
+			</ul>}
 		</div>
 	)
 }
