fa9682c24498e5ba8fcc752107aa4aea3427a844
Dejan Vjestica <dvjestica@gmail.com>
2022-06-27 13:06:31 +0200

    Add  spinner component

---
+++ src/components/UI/Spinner/Spinner.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// styles
+import styles from './Spinner.module.scss'
+
+// types
+type SpinnerProps = {
+	children?: React.ReactNode
+}
+
+export const Spinner = ({ children = '... Loading' }: SpinnerProps): JSX.Element => {
+	return (
+		<div className={styles.loader__wrapper}>
+			<div className={styles.loader__inner}></div>
+			<p className={styles.loader__text}>{children}</p>
+		</div>
+	)
+}
