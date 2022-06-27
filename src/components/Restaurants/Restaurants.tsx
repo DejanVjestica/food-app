216b29a8913250ec499f3ee08f67b2870e094c7d
Dejan Vjestica <dvjestica@gmail.com>
2022-06-27 10:59:50 +0200

    Add restaurants list component

--- src/components/Restaurants/Restaurants.tsx
+++ src/components/Restaurants/Restaurants.tsx
@@ -1,9 +1,54 @@
 import React from 'react'
 
-export const Restaurants = () => {
+// components
+import { Wrapper } from '../Helpers/Wrapper/Wrapper'
+import { Img } from '../UI/Img/Img'
+import { ListItem } from '../UI/ListItem/ListItem'
+import { Button } from '../UI/Button/Button'
+
+// styles
+import styles from './Restaurants.module.scss'
+
+// firebase
+import { getDatabase, ref as databaseRef } from 'firebase/database'
+import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage'
+import { useList } from 'react-firebase-hooks/database'
+
+export const Restaurants = (): JSX.Element => {
+	const dbRef = databaseRef(getDatabase(), 'restaurants')
+	const storage = getStorage()
+
+	// state
+	const [snapshots] = useList(dbRef)
+	const restaurantsList = snapshots?.map(snapshot => snapshot.val())
+
+	const restaurants = restaurantsList?.map((restaurant) => {
+		const imagePath = `restaurants/${restaurant.cover}`
+
+		const getImageUrl: () => void = () => {
+			getDownloadURL(storageRef(storage, imagePath)).then(url => {
+				const img = document.getElementById(`${restaurant.name}`)
+				img?.setAttribute('src', url)
+			})
+		}
+		getImageUrl()
+
+		return (
+			<ListItem className={styles['restaurant-item']} key={restaurant.name}>
+				<Img id={restaurant.name} alt={restaurant.name} />
+				<Wrapper as="div" >
+					<h3>{restaurant.name}</h3>
+					<p>{restaurant.shortDesription}</p>
+				</Wrapper>
+			</ListItem>
+		)
+	})
+
 	return (
-		<div>
-			<h1>Restaurants</h1>
+		<div className={styles.restaurants}>
+			<ul className={styles['restaurants-list']}>
+				{restaurants}
+			</ul>
 		</div>
 	)
 }
