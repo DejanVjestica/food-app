a4808ab6372445b88c96f6e86284856d75ae517d
Dejan Vjestica <dvjestica@gmail.com>
2022-06-27 10:16:23 +0200

    Refactor button to polimorphic component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,14 +1,56 @@
-import React from 'react'
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import type { LinkProps } from 'react-router-dom'
+
 import styles from './Button.module.scss'
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-	children: React.ReactNode,
+type BaseProps = {
+	children: React.ReactNode
+	className?: string
 	variant?: 'primary' | 'secondary' | 'icon' | 'simple'
 	modifier?: string,
-} & Omit<React.ComponentProps<'button'>, 'children'>
+}
 
-export const Button = (props: ButtonProps) => {
-	const { variant, modifier } = props
+type ButtonAsButton = BaseProps &
+  Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, keyof BaseProps> & {
+    as?: 'button'
+  }
 
-	return <button className={[styles.default, styles[`${variant}`], styles[`${modifier}`]].join(' ')} {...props}>{props.children}</button>
+type ButtonAsLink = BaseProps &
+  Omit<LinkProps, keyof BaseProps> & {
+    as: 'link'
+  }
+
+type ButtonAsExternal = BaseProps &
+  Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof BaseProps> & {
+    as: 'a'
+  }
+
+type ButtonProps =
+  | ButtonAsButton
+  | ButtonAsExternal
+  | ButtonAsLink
+
+export const Button = (props: ButtonProps): JSX.Element => {
+	const className = [styles.default, styles[`${props.variant}`], styles[`${props.modifier}`]].join(' ')
+	if (props.as === 'link') {
+		// don't pass unnecessary props to component
+		const { children, ...rest } = props
+		return <Link className={className} {...rest}>{children}</Link>
+	} else if (props.as === 'a') {
+		const { children, ...rest } = props
+		return (
+			<a
+				className={className}
+				target='_blank'
+				rel='noopener noreferrer'
+				{...rest}
+			>
+				{children}
+			</a>
+		)
+	} else {
+		const { children, ...rest } = props
+		return <button className={className} {...rest}>{children}</button>
+	}
 }
