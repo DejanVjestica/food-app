e58b4a6539209fefb07042f55fa5ec5bd2d8ca9c
Dejan Vjestica <dvjestica@gmail.com>
2022-06-28 15:20:51 +0200

    Add welcome component

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 
+// components
+import { Button } from '../UI/Button/Button'
+import { Stage } from '../Layout/Stage/Stage'
+
+// firebase
+import { getDatabase, ref as databaseRef } from 'firebase/database'
+import { useObject } from 'react-firebase-hooks/database'
+
+// types
+import { SrcSetItem } from '../../types/use-srcSet.types'
+
+// styles
+import styles from './Welcome.module.scss'
+
 export const Welcome = () => {
+	const dbRef = databaseRef(getDatabase(), 'restaurants/info/cover')
+	const [snapshot] = useObject(dbRef)
+	const cover: SrcSetItem[] = snapshot?.val()
+
 	return (
-		<div>
-			<h1>Welcome to our food portal</h1>
-			<p>Please log in to continue</p>
+		<div className={styles.welcome}>
+			<div className={styles.welcome__info}>
+				<h1>Welcome to best food portal</h1>
+				<Button as="link" to="/restaurants" variant='primary'>
+					<span>Please check our restaurants</span>
+				</Button>
+			</div>
+			<Stage data={cover}></Stage>
 		</div>
 	)
 }
