6052632457b0ea71cdc797f328821c1149f4b122
Dejan Vjestica <dvjestica@gmail.com>
2022-06-29 16:06:17 +0200

    get meals from firebase

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 // components
 import { MealItem } from './MealItem/MealItem'
@@ -6,15 +7,40 @@ import { MealItem } from './MealItem/MealItem'
 // styles
 import styles from './AvailableMeals.module.scss'
 
-// data
-import Meals from './Meals.data.json'
+// firebase
+import { getDatabase, ref as databaseRef } from 'firebase/database'
+import { useObject } from 'react-firebase-hooks/database'
+
+// types
+import { MealItemType } from '../../types/cart.types'
 
 export const AvailableMeals = () => {
-	const meals = Meals.map((meal) => <MealItem key={meal.id} meal={meal} />)
+	// get params from url
+	const params = useParams()
+	const id = params.id
+	// get meals from firebase
+	const dbRef = databaseRef(getDatabase(), 'restaurants/info/shops/' + id + '/menu')
+	const [snapshot] = useObject(dbRef)
+	const menu: MealItemType[] = snapshot?.val()
+	// state
+	const [meals, setMeals] = useState<JSX.Element[]>([])
+
+	useEffect(() => {
+		if (!menu) return
+		const menuItems = menu.map((meal: MealItemType, index) => {
+			const newMeal = {
+				...meal,
+				id: parseInt(id as string)
+			}
+			return <MealItem key={index} meal={newMeal} />
+		})
+		setMeals(menuItems)
+	}
+	, [snapshot])
 
 	return (
-		<section className={styles['available-meals']}>
+		<article className={styles['available-meals']}>
 			<ul className={styles['available-meals__list']}>{meals}</ul>
-		</section>
+		</article>
 	)
 }
