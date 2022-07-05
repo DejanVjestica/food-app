094fa9d6fb1f13ef10a097fc998ebf7eb4771be7
Dejan Vjestica <dvjestica@gmail.com>
2022-07-05 14:21:33 +0200

    Add tag button component

---
+++ src/components/UI/Tags/TagButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// styles
+import styles from './TagButton.module.scss'
+
+// types
+type TagButtonProps = {
+	children?: React.ReactNode
+	onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+	active?: boolean
+} & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'button'>
+
+export const TagButton = ({ children, active, ...rest }: TagButtonProps): JSX.Element => {
+	const className = [styles['tag-button'], styles[`${active ? 'is-active' : ''}`]].join(' ')
+	return (
+		<button className={className} {...rest}>
+			{children}
+		</button>
+	)
+}
