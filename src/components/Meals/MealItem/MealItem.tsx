9ac7c5ba014ab1861343424deb259dd4d5b19d65
Dejan Vjestica <dvjestica@gmail.com>
2022-07-06 16:36:31 +0200

    Add spinner to mealItem

--- src/components/Meals/MealItem/MealItem.tsx
+++ src/components/Meals/MealItem/MealItem.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 // components
 import { Img } from '../../UI/Img/Img'
 import { MealItemForm } from './MealItemForm'
+import { Spinner } from '../../UI/Spinner/Spinner'
 
 // styles
 import styles from './MealItem.module.scss'
@@ -15,11 +16,24 @@ type MealItemProps = {
 }
 
 export const MealItem = (props: MealItemProps) => {
+	// state
+	const [isLoading, setIsLoading] = useState(true)
+	// ref
+	const imageWrapper = useRef<HTMLDivElement>(null)
 	const price = `€${props.meal.price.toFixed(2)}`
+
+	useEffect(() => {
+		const img = imageWrapper.current?.querySelector('img')
+		img?.addEventListener('load', () => {
+			setIsLoading(false)
+		})
+	}
+	, [imageWrapper])
 
 	return (
 		<li className={styles['meal-item__list-item']}>
-			<div className={styles['meal-item__image']}>
+			<div ref={imageWrapper} className={styles['meal-item__image']}>
+				{isLoading && <Spinner></Spinner>}
 				<Img src={props.meal.image.src} alt={props.meal.image.alt} srcsetdata={props.meal.image.srcSet} />
 			</div>
 			<div className={styles['meal-item__content']}>
