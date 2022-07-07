9517b1af04b10146ae1749e8e35231b0f4e2804d
Dejan Vjestica <dvjestica@gmail.com>
2022-07-07 11:42:14 +0200

    Refactor firebase file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,8 @@
-/* eslint-disable no-tabs */
-/* eslint-disable space-before-function-paren */
 import { initializeApp } from 'firebase/app'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
 import {
 	GoogleAuthProvider,
 	getAuth,
@@ -10,56 +12,54 @@ import {
 	sendPasswordResetEmail,
 	signOut,
 	onAuthStateChanged,
-	sendEmailVerification,
-	updateProfile
+	sendEmailVerification
 } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-import { getDatabase, ref, set } from 'firebase/database'
+
+import { getFirestore, doc, updateDoc, collection, setDoc, getDoc, getDocs, where, query } from 'firebase/firestore'
 
 // types
 import { MealItemType } from './types/cart.types'
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY
-
 const firebaseConfig = {
-	apiKey: 'AIzaSyD-5RbSwbB_eaBEh10qK17BI_bhVets8u8',
-	authDomain: 'food-app-ed31b.firebaseapp.com',
-	databaseURL: 'https://food-app-ed31b-default-rtdb.europe-west1.firebasedatabase.app',
-	projectId: 'food-app-ed31b',
-	storageBucket: 'food-app-ed31b.appspot.com',
-	messagingSenderId: '809842321837',
-	appId: '1:809842321837:web:645a198be14e831bd29924',
-	measurementId: 'G-KWF8EBZ9QX'
+	apiKey: process.env.REACT_APP_FB_API_KEY,
+	authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+	projectId: process.env.REACT_APP_FB_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FB_MSID,
+	appId: process.env.REACT_APP_FB_APP_ID,
+	measurementId: process.env.REACT_APP_FB_APP_MESURMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-const db = getFirestore(app)
 
-const database = getDatabase()
+const db = getFirestore()
 
 const googleProvider = new GoogleAuthProvider()
 
-const signInWithGoogle = async () => {
+const signInWithGoogle = async() => {
 	try {
-		const res = await signInWithPopup(auth, googleProvider)
-		const user = res.user
-		await set(ref(database, `users/${user.uid}/username`), {
-			name: user.displayName,
-			email: user.email,
-			photoUrl: user.photoURL,
-			isVerified: user.emailVerified,
-			isAnonymous: user.isAnonymous,
-			phoneNumber: user.phoneNumber
-		})
+		await signInWithPopup(auth, googleProvider)
+		const data = {
+			name: auth.currentUser?.displayName,
+			email: auth.currentUser?.email,
+			photoUrl: auth.currentUser?.photoURL,
+			createdAt: Date.now(),
+			isVerified: auth.currentUser?.emailVerified,
+			isAnonymous: auth.currentUser?.isAnonymous,
+			phoneNumber: auth.currentUser?.phoneNumber
+		}
+
+		const userId = auth.currentUser?.uid ? auth.currentUser.uid : 'new-user-id'
+		await setDoc(doc(db, 'users', userId), data)
 	} catch (err: any) {
 		console.error(err)
 		alert(err.message)
 	}
 }
 
-const logInWithEmailAndPassword = async (email: string, password: string) => {
+const logInWithEmailAndPassword = async(email: string, password: string) => {
 	try {
 		await signInWithEmailAndPassword(auth, email, password)
 	} catch (err: any) {
@@ -68,31 +68,28 @@ const logInWithEmailAndPassword = async (email: string, password: string) => {
 	}
 }
 
-const registerWithEmailAndPassword = async (name: string, email: string, password: string) => {
+const registerWithEmailAndPassword = async(name: string, email: string, password: string) => {
 	try {
 		const res = await createUserWithEmailAndPassword(auth, email, password)
-		const user = res.user
+		await sendEmailVerification(res.user)
 
-		await updateProfile(user, {
-			displayName: name
-		})
-
-		await set(ref(database, `users/${user.uid}/username`), {
+		const data = {
 			name,
 			email,
-			photoUrl: user.photoURL,
-			isVerified: user.emailVerified,
-			isAnonymous: user.isAnonymous,
-			phoneNumber: user.phoneNumber
-		})
-		await sendEmailVerification(user)
+			photoUrl: res.user.photoURL,
+			createdAt: Date.now(),
+			isAnonymous: res.user.isAnonymous,
+			phoneNumber: res.user.phoneNumber
+		}
+
+		await setDoc(doc(db, 'users', res.user.uid), data)
 	} catch (err: any) {
 		console.error(err)
 		alert(err.message)
 	}
 }
 
-const sendPasswordReset = async (email: string) => {
+const sendPasswordReset = async(email: string) => {
 	try {
 		await sendPasswordResetEmail(auth, email)
 		alert('Password reset link sent!')
@@ -115,26 +112,48 @@ type OrderType = {
 	order: MealItemType[]
 }
 
-const writeOrder = async (order: OrderType) => {
-	const db = getDatabase()
-	const auth = getAuth()
-	const user: string = auth.currentUser?.uid || ''
-	const randomId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-	const path = user ? `orders/${user}` : `orders/${randomId}`
+const writeOrder = async(order: OrderType) => {
+	if (!auth.currentUser) {
+		alert('You must be logged in to place an order!')
+		return
+	}
+
+	const userId: string = await auth.currentUser.uid
+
+	const data = {
+		streetName: order.streetName,
+		hausNumber: order.hausNumber,
+		name: order.name,
+		email: order.email,
+		telefon: order.phone,
+		order: order.order
+	}
 
 	try {
-		await set(ref(db, path), {
-			streetName: order.streetName,
-			hausNumber: order.hausNumber,
-			name: order.name,
-			email: order.email,
-			telefon: order.phone,
-			order: order.order
-		})
+		await setDoc(doc(db, 'orders', userId), data)
 	} catch (err: any) {
 		console.error(err)
 		alert(err.message)
 	}
 }
 
-export { auth, db, signInWithGoogle, logInWithEmailAndPassword, registerWithEmailAndPassword, sendPasswordReset, onAuthStateChanged, logout, writeOrder }
+export {
+	firebase,
+	db,
+	auth,
+	getDoc,
+	getDocs,
+	collection,
+	doc,
+	setDoc,
+	updateDoc,
+	where,
+	query,
+	signInWithGoogle,
+	logInWithEmailAndPassword,
+	registerWithEmailAndPassword,
+	sendPasswordReset,
+	onAuthStateChanged,
+	logout,
+	writeOrder
+}
